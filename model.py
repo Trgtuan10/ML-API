e7ce2f1e61133ecaefc2873374182f81956f4f27
Trgtuan10 <tuannguyentrong.402@gmail.com>
2024-08-05 11:01:06 +0700

    add testing

---
+++ model.py
@@ -0,0 +1,26 @@
+from transformers import ViltProcessor, ViltForQuestionAnswering
+from PIL import Image
+import requests
+from PIL import Image
+
+# 470MB
+processor = ViltProcessor.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+model = ViltForQuestionAnswering.from_pretrained("dandelin/vilt-b32-finetuned-vqa")
+
+
+def model_pipeline(text: str, image: Image):
+    # prepare inputs
+    encoding = processor(image, text, return_tensors="pt")
+
+    # forward pass
+    outputs = model(**encoding)
+    logits = outputs.logits
+    idx = logits.argmax(-1).item()
+
+    return  model.config.id2label[idx]
+
+if __name__ == "__main__":
+    url = "http://images.cocodataset.org/val2017/000000039769.jpg"
+    image = Image.open(requests.get(url, stream=True).raw)
+    text = "How many cats are there?"
+    print(model_pipeline(text, image))
